abed36f74e4cb6489ac66a4aaa06decfeb415646
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-21 21:37:18 +0900

    カレンダーの権限を全て取得

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            Rails.application.credentials.google[:client_id],
            Rails.application.credentials.google[:client_secret],
-           scope: 'email,profile,https://www.googleapis.com/auth/calendar.events',
+           scope: 'email,profile,https://www.googleapis.com/auth/calendar',
            access_type: 'offline',
            prompt: 'consent'
 end
