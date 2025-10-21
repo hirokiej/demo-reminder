0cbc4ee5e8ef21da8e189a0bcbe14091b9c46bf3
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-21 13:28:32 +0900

    友達からメッセージが来た時にfriendsにlne_idとdisplay_nameが登録される

--- app/controllers/linewebhook_controller.rb
+++ app/controllers/linewebhook_controller.rb
@@ -1,15 +1,23 @@
+require 'line/bot'
+
 class LinewebhookController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :check_logged_in, only: [:callback]
+
   def callback
     body = request.body.read
-    events = client.parse_event_from(body)
+    signature = request.headers['X-Line-Signature']
+
+    line_account = find_line_account_by_signature(body, signature)
+
+    client = Line::Bot::V2::MessagingApi::ApiClient.new(channel_access_token: line_account.line_channel_access_token)
+    events = JSON.parse(body)['events']
 
     events.each do |event|
-      case event
-      when Line::Bot::Event::Follow
-        handle_follow(event)
-      when Line::Bot::Event::Message
-        handle_message(event)
-      end
+      user_id = event.dig('source', 'userId')
+      next unless user_id
+      
+      handle_follow(user_id, line_account, client)
     end
 
     head :ok
@@ -17,27 +25,28 @@ class LinewebhookController < ApplicationController
 
   private
 
-  def client
-    @client ||= Line::Bot::Client.new do |config|
-      config.channel_secret = current_user.line_channel_secret
-      config.channel_token = current_user.line_channel_access_token
+  def find_line_account_by_signature(body,signature)
+    User.find_each do |account|
+      next unless account.line_channel_secret.present?
+
+      if valid_signature?(body,signature, account.line_channel_secret)
+        return account
+      end
     end
+    nil
   end
 
-  def handle_follow(event)
-    user_id = event.source.user_id
-    return if current_user.friends.exists?(line_user_id: user_id)
-    profile = JSON.parse(client.get_profile(user_id).body)
-    display_name = profile['displayName']
-    current_user.friends.create(line_user_id: user_id, line_display_name: display_name)
+  def valid_signature?(body,signature, channel_secret)
+    hash = OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, channel_secret, body)
+    Base64.strict_encode64(hash) == signature
   end
 
-  def handle_message(event)
-    user_id = event.source.user_id
+  def handle_follow(user_id, line_account, client)
+    return if line_account.friends.exists?(line_user_id: user_id)
 
-    unless current_user.friends.exists?(line_user_id: user_id)
-      handle_follow(event)
-    end
-    client.reply_message(event['replyToken'], { type: 'text', text: "おk" })
+    profile = client.get_profile(user_id: user_id)
+    display_name = profile.display_name
+
+    line_account.friends.create(line_user_id: user_id, line_display_name: display_name)
   end
 end
